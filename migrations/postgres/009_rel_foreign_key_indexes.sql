46b2ca3242c91098ea13bfb25ea21c7d07e61f68
caffix <caffix@users.noreply.github.com>
2024-08-16 19:01:08 -0400

    added indexes for foreign keys in the relations table

---
+++ migrations/postgres/009_rel_foreign_key_indexes.sql
@@ -0,0 +1,9 @@
+-- +migrate Up
+
+CREATE INDEX idx_rel_from_asset_id ON relations (from_asset_id);
+CREATE INDEX idx_rel_to_asset_id ON relations (to_asset_id);
+
+-- +migrate Down
+
+DROP INDEX IF EXISTS idx_rel_from_asset_id;
+DROP INDEX IF EXISTS idx_rel_to_asset_id;
