cbaa3d5a2539201ba77d59b184dca0c344337e65
caffix <caffix@users.noreply.github.com>
2025-12-29 19:48:29 -0500

    added the extensions

--- initdb-assetdb.sh
+++ initdb-assetdb.sh
@@ -18,6 +18,9 @@ EOSQL
 # extension and grant privileges to the Amass user
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$AMASS_DB" <<-EOSQL
     \getenv username AMASS_USER
+    CREATE EXTENSION citext SCHEMA public;
+    CREATE EXTENSION pg_trgm SCHEMA public;
+    CREATE EXTENSION btree_gin SCHEMA public;
 	GRANT USAGE ON SCHEMA public TO :username;
     GRANT CREATE ON SCHEMA public TO :username;
     GRANT ALL ON ALL TABLES IN SCHEMA public TO :username;
